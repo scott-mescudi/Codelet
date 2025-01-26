87e0a50acf4df1251d75f3219ba7f260362d3b00
script kiddie <anish.chittu2007@gmail.com>
2025-01-26 01:46:45 +0100

    Refactor Home component layout to use grid for better spacing and add additional CodeBox instances

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -30,7 +30,9 @@ if err := http.ListenAndServe(":8080", app); err != nil {
 export default function Home() {
   return (
     <>
-    <div className="w-full h-full flex  items-center justify-center">
+    <div className="w-full h-full grid gap-5  items-center justify-center">
+      <CodeBox fileName="main" extension=".jsx" code={code} />
+      <CodeBox fileName="main" extension=".jsx" code={code} />
       <CodeBox fileName="main" extension=".jsx" code={code} />
     </div>
     </>
