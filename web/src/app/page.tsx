96c48956f446e5f6a000f2a739c521281877d9ca
script kiddie <anish.chittu2007@gmail.com>
2025-01-31 21:36:29 +0100

    Update Home component layout to use flexbox for better spacing

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -43,7 +43,7 @@ db, err = dataAccess.PrepareStatements(query, db)
 export default function Home() {
   return (
     <>
-    <div className="sm:w-1/2 sm:p-x0 w-full px-5">
+    <div className="sm:w-1/2 flex flex-col gap-5 sm:p-x0 w-full px-5">
       <CodeBox code={code} />
     </div>
     </>
