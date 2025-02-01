10536de333d9b1f70c0b9331e8f58f75ddb9f373
script kiddie <anish.chittu2007@gmail.com>
2025-02-01 18:45:49 +0100

    Add link prop to DropdownItem component for dynamic navigation

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -19,6 +19,7 @@ interface DropdownItemProps {
     subTitle: string
     iconBgColor?: string
     iconColor?:string 
+    link?: string
 
     iconBgHoverColor?: string
     iconHoverColor?: string
@@ -26,14 +27,14 @@ interface DropdownItemProps {
 
 }
 
-function DropdownItem({title, subTitle, iconBgColor="bg-black", iconColor="text-white", iconBgHoverColor="group-hover:bg-white", iconHoverColor="group-hover:text-black", icon}:DropdownItemProps) {
+function DropdownItem({title, link="#", subTitle, iconBgColor="bg-black", iconColor="text-white", iconBgHoverColor="group-hover:bg-white", iconHoverColor="group-hover:text-black", icon}:DropdownItemProps) {
     return (
         <>
             <div className="h-12 group flex flex-row rounded-md w-52 gap-3">
                 <div className={`h-full aspect-square flex items-center justify-center ${iconBgColor} rounded-sm ${iconColor} ${iconHoverColor} duration-300 ease-out border border-white border-opacity-15 ${iconBgHoverColor}`}>
                     {icon}
                 </div>
-                <Link href={"https://github.com/scott-mescudi"} className="w-full h-full flex flex-col overflow-hidden">
+                <Link href={link} className="w-full h-full flex flex-col overflow-hidden">
                     <h1 className="text-lg  truncate text-white font-semibold">{title}</h1>
                     <p  className="text-sm text-white text-opacity-50 group-hover:text-opacity-100 duration-200 ease-in-out overflow-hidden">{subTitle}</p>
                 </Link>
