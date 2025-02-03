2799ab811908118b941bc7cc7d364325dc7ade1d
script kiddie <anish.chittu2007@gmail.com>
2025-02-03 23:55:08 +0100

    Remove unused CPU profiling and HTTP server for performance optimization

--- server/main.go
+++ server/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
-	"runtime/pprof"
 
 	srv "github.com/scott-mescudi/codelet/service"
 )
@@ -13,22 +11,6 @@ func main() {
 	if err := os.MkdirAll("/src/logs", 0755); err != nil {
 		log.Fatalln("Failed to create logs directory")
 	}
-
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
-
-	f, err := os.Create("/src/logs/cpu.prof")
-	if err != nil {
-		log.Fatal(err)
-
-	}
-	defer f.Close()
-
-	if err := pprof.StartCPUProfile(f); err != nil {
-		log.Fatal(err)
-	}
-	defer pprof.StopCPUProfile()
 
 	srv.NewCodeletServer()
 }
