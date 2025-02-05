88a5d89b8dc8d2c83659df50528ab1df98e9f47c
script kiddie <anish.chittu2007@gmail.com>
2025-02-05 23:34:28 +0100

    Add fallback icon for unsupported languages; update logoMap to use lowercase keys for consistency

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -14,12 +14,15 @@ interface ErrorResponse {
 }
 
 const logoMap = new Map<string, string>();
-logoMap.set("golang", "https://devicon-website.vercel.app/api/go/original.svg")
-logoMap.set("Ruby", "https://devicon-website.vercel.app/api/ruby/original.svg")
-logoMap.set("Python", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg")
-logoMap.set("JavaScript", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg")
-logoMap.set("c++", "")
-logoMap.set("c", "")
+logoMap.set("go", "https://devicon-website.vercel.app/api/go/original.svg")
+logoMap.set("ruby", "https://devicon-website.vercel.app/api/ruby/original.svg")
+logoMap.set("python", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg")
+logoMap.set("javascript", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg")
+logoMap.set("swift", "https://devicon-website.vercel.app/api/swift/original.svg")
+logoMap.set("php", "https://devicon-website.vercel.app/api/php/original.svg")
+logoMap.set("java", "https://devicon-website.vercel.app/api/java/original.svg")
+logoMap.set("c", "https://devicon-website.vercel.app/api/c/original.svg")
+logoMap.set("c#", "https://devicon-website.vercel.app/api/csharp/original.svg")
 
 
 type CodeSnippets  = CodeSnippet[]
@@ -50,14 +53,23 @@ export default async function Home() {
     return <div>Error: {snippets.error} (Code: {snippets.code})</div>;
   }
 
+  const getIcon = (name:string) => {
+    const logo = logoMap.get(name.toLowerCase())
+    if (!logo) {
+      return "/fallback.svg"
+    }
+
+    return logo
+  }
+
   return (
     <>
       <div className="px-3 sm:w-2/3 mt-10 flex flex-col gap-5 w-full h-full">
-        <div className="w-full h-fit lg:grid lg:gap-10 2xl:grid-cols-3 grid-cols-2 gap-5 justify-center  flex flex-col">
+        <div className="w-full h-fit lg:grid pb-10 lg:gap-10 2xl:grid-cols-3 grid-cols-2 gap-5 justify-center  flex flex-col">
           {snippets.map((snippet, idx) => (
             <div key={idx} className="w-full h-40 select-none hover:scale-105 duration-300 hover:cursor-pointer ease-in-out will-change-transform rounded-lg bg-black hover:bg-neutral-900 border gap-3 border-white border-opacity-15 p-3 flex flex-row">
               <div className="w-1/5 aspect-square rounded-md overflow-hidden">
-                <img className="h-full w-full" src={logoMap.get(snippet.language)}></img>
+                <img className="h-full w-full" src={getIcon(snippet.language)}></img>
               </div>
               <div className="w-4/5 h-full gap-1 flex flex-col">
                 <div className="w-full pt-2 h-3/4 ">
