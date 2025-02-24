004d6e0a882cd3475dde4d7a9c365ba4c0d4df50
script kiddie <anish.chittu2007@gmail.com>
2025-02-24 20:39:29 +0100

    made it more respoinsive

--- web/src/app/(main)/page.tsx
+++ web/src/app/(main)/page.tsx
@@ -34,9 +34,9 @@ export default function Home() {
   return (
     <>
       <div className="w-full flex flex-col items-center">
-        <div className="h-fit overflow-hidden w-full sm:w-2/3 relative flex flex-col justify-center border border-t-0 border-r-0 border-white/5">
+        <div className="h-fit overflow-hidden w-full lg:w-2/3 relative flex flex-col justify-center border border-t-0 border-r-0 border-white/5">
           <div className="h-full w-full mb-20 mt-10 flex flex-row items-center p-5">
-            <div className="h-3/4 sm:w-1/2 flex  flex-col gap-2">
+            <div className="h-3/4 lg:w-1/2 flex  flex-col gap-2">
               <p className="w-full text-white font-bold text-5xl">
                 All Your Code Snippets,{" "}
                 <span className="text-green-700">Organized</span>
@@ -52,7 +52,7 @@ export default function Home() {
                 Create a account
               </Link>
             </div>
-            <div className="h-3/4 relative w-1/2 px-2 hidden sm:flex flex-col gap-2">
+            <div className="h-3/4 relative w-1/2 px-2 hidden lg:flex flex-col gap-2">
               <CodeBox background="bg-neutral-950" code={code} />
               <CodeBox background="bg-orange-950" code={code2} />
               <CodeBox background="bg-blue-950" code={code3} />
@@ -61,14 +61,14 @@ export default function Home() {
           <LinesBackground />
         </div>
 
-        <div className="h-fit w-full sm:w-2/3 relative flex flex-col items-center border border-t-0  border-white/5">
+        <div className="h-fit w-full lg:w-2/3 relative flex flex-col items-center border border-t-0  border-white/5">
           <p className="w-fit px-4 text-center border border-y-0 border-x-white/5 py-2 text-white font-bold  text-4xl">
             Syntax highlighting for all programming languages
           </p>
           <InfiniteScrollAnimationPage />
         </div>
 
-        <div className="h-fit w-full sm:w-2/3 pb-5 flex flex-col items-center border-b-0 border border-t-0  border-white/5">
+        <div className="h-fit w-full lg:w-2/3 pb-5 flex flex-col items-center border-b-0 border border-t-0  border-white/5">
           <p className="w-fit px-4 text-center border border-y-0 border-x-white/5 py-2 text-white font-bold  text-4xl">
             Simple ui
           </p>
