37e6edad85f64a066c3e4d3b12b1b21663462bb0
script kiddie <anish.chittu2007@gmail.com>
2025-02-27 21:13:11 +0100

    update api routes

--- web/src/shared/api/SnippetApiReq.tsx
+++ web/src/shared/api/SnippetApiReq.tsx
@@ -1,222 +1,225 @@
 interface ErrorResp {
-	error: string
-	code: number
+  error: string;
+  code: number;
 }
 interface CodeSnippet {
-	id: number
-	language: string
-	title: string
-	code: string
-	favorite: boolean
-	private: boolean
-	tags: string[]
-	description: string
+  id: number;
+  language: string;
+  title: string;
+  code: string;
+  favorite: boolean;
+  private: boolean;
+  tags: string[];
+  description: string;
 }
 
 interface SmallSnippet {
-	id: number
-	language: string
-	title: string
-	favorite: boolean
+  id: number;
+  language: string;
+  title: string;
+  favorite: boolean;
 }
 
 interface CodeSnippetReq {
-	language: string
-	title: string
-	code: string
-	favorite?: boolean
-	private?: boolean
-	tags?: string[]
-	description?: string
+  language: string;
+  title: string;
+  code: string;
+  favorite?: boolean;
+  private?: boolean;
+  tags?: string[];
+  description?: string;
 }
 
-type SmallSnippets = SmallSnippet[]
+type SmallSnippets = SmallSnippet[];
 
 export async function GetSnippetByID(
-	snippetID: number,
-	token: string
+  snippetID: number,
+  token: string
 ): Promise<CodeSnippet | undefined> {
-	if (!snippetID || snippetID < 0) {
-		return undefined
-	}
+  if (!snippetID || snippetID < 0) {
+    return undefined;
+  }
 
-	if (token === '') {
-		return undefined
-	}
+  if (token === "") {
+    return undefined;
+  }
 
-	try {
-		const resp = await fetch(
-			`http://localhost:3021/api/v1/user/snippets/${snippetID}`,
-			{
-				method: 'GET',
-				headers: {
-					Authorization: token
-				}
-			}
-		)
+  try {
+    const resp = await fetch(
+      `https://codeletserver-production.up.railway.app/api/v1/user/snippets/${snippetID}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
 
-		if (!resp.ok) {
-			const errResp = (await resp.json()) as ErrorResp
-			console.log(errResp)
-			return undefined
-		}
+    if (!resp.ok) {
+      const errResp = (await resp.json()) as ErrorResp;
+      console.log(errResp);
+      return undefined;
+    }
 
-		const snippets = (await resp.json()) as CodeSnippet
-		return snippets
-	} catch (err) {
-		console.error(err)
-		return undefined
-	}
+    const snippets = (await resp.json()) as CodeSnippet;
+    return snippets;
+  } catch (err) {
+    console.error(err);
+    return undefined;
+  }
 }
 
 export async function getSmallSnippets(
-	token: string
+  token: string
 ): Promise<SmallSnippets | undefined> {
-	if (token === '') {
-		return undefined
-	}
+  if (token === "") {
+    return undefined;
+  }
 
-	try {
-		const resp = await fetch(
-			'http://localhost:3021/api/v1/user/small/snippets',
-			{
-				method: 'GET',
-				headers: {
-					Authorization: token
-				}
-			}
-		)
+  try {
+    const resp = await fetch(
+      "https://codeletserver-production.up.railway.app/api/v1/user/small/snippets",
+      {
+        method: "GET",
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
 
-		if (!resp.ok) {
-			return undefined
-		}
+    if (!resp.ok) {
+      return undefined;
+    }
 
-		const snippets = (await resp.json()) as SmallSnippets
-		if (snippets.length <= 0) {
-			return undefined
-		}
+    const snippets = (await resp.json()) as SmallSnippets;
+    if (snippets.length <= 0) {
+      return undefined;
+    }
 
-		return snippets
-	} catch (err) {
-		console.error(err)
-		return undefined
-	}
+    return snippets;
+  } catch (err) {
+    console.error(err);
+    return undefined;
+  }
 }
 
 export async function DeleteReq(
-	token: string,
-	id: number
+  token: string,
+  id: number
 ): Promise<boolean | undefined> {
-	if (token === '') {
-		return undefined
-	}
+  if (token === "") {
+    return undefined;
+  }
 
-	try {
-		const resp = await fetch(
-			`http://localhost:3021/api/v1/user/snippets/${id}`,
-			{
-				method: 'DELETE',
-				headers: {
-					Authorization: token
-				}
-			}
-		)
+  try {
+    const resp = await fetch(
+      `https://codeletserver-production.up.railway.app/api/v1/user/snippets/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
 
-		return resp.ok
-	} catch (err) {
-		console.error(err)
-		return undefined
-	}
+    return resp.ok;
+  } catch (err) {
+    console.error(err);
+    return undefined;
+  }
 }
 
 export async function addSnippetReq(
-	token: string,
-	language: string,
-	title: string,
-	tags: string[],
-	description: string,
-	code: string
+  token: string,
+  language: string,
+  title: string,
+  tags: string[],
+  description: string,
+  code: string
 ): Promise<boolean | undefined> {
-	if (token === '' || title === '' || language === '' || code === '') {
-		return undefined
-	}
+  if (token === "" || title === "" || language === "" || code === "") {
+    return undefined;
+  }
 
-	const body: CodeSnippetReq = {
-		language,
-		title,
-		code,
-		favorite: false,
-		private: true,
-		tags,
-		description
-	}
+  const body: CodeSnippetReq = {
+    language,
+    title,
+    code,
+    favorite: false,
+    private: true,
+    tags,
+    description,
+  };
 
-	try {
-		const resp = await fetch('http://localhost:3021/api/v1/user/snippets', {
-			method: 'POST',
-			headers: {
-				Authorization: token,
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(body)
-		})
+  try {
+    const resp = await fetch(
+      "https://codeletserver-production.up.railway.app/api/v1/user/snippets",
+      {
+        method: "POST",
+        headers: {
+          Authorization: token,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      }
+    );
 
-		if (!resp.ok) {
-			console.log(await resp.json())
-			return false
-		}
+    if (!resp.ok) {
+      console.log(await resp.json());
+      return false;
+    }
 
-		return true
-	} catch (err) {
-		console.error(err)
-		return undefined
-	}
+    return true;
+  } catch (err) {
+    console.error(err);
+    return undefined;
+  }
 }
 
 export async function updateSnippetReq(
-	id: number,
-	token: string,
-	language: string,
-	title: string,
-	tags: string[],
-	description: string,
-	code: string
+  id: number,
+  token: string,
+  language: string,
+  title: string,
+  tags: string[],
+  description: string,
+  code: string
 ): Promise<boolean | undefined> {
-	if (token === '' || title === '' || language === '' || code === '') {
-		return undefined
-	}
+  if (token === "" || title === "" || language === "" || code === "") {
+    return undefined;
+  }
 
-	const body: CodeSnippetReq = {
-		language,
-		title,
-		code,
-		favorite: false,
-		private: true,
-		tags,
-		description
-	}
+  const body: CodeSnippetReq = {
+    language,
+    title,
+    code,
+    favorite: false,
+    private: true,
+    tags,
+    description,
+  };
 
-	try {
-		const resp = await fetch(
-			`http://localhost:3021/api/v1/user/snippets/${id}`,
-			{
-				method: 'PUT',
-				headers: {
-					Authorization: token,
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify(body)
-			}
-		)
+  try {
+    const resp = await fetch(
+      `https://codeletserver-production.up.railway.app/api/v1/user/snippets/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: token,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      }
+    );
 
-		if (!resp.ok) {
-			console.log(await resp.json())
-			return false
-		}
+    if (!resp.ok) {
+      console.log(await resp.json());
+      return false;
+    }
 
-		return true
-	} catch (err) {
-		console.error(err)
-		return undefined
-	}
+    return true;
+  } catch (err) {
+    console.error(err);
+    return undefined;
+  }
 }
--- web/src/shared/api/UserApiReq.tsx
+++ web/src/shared/api/UserApiReq.tsx
@@ -1,134 +1,145 @@
 interface LoginRequest {
-	email: string
-	password: string
+  email: string;
+  password: string;
 }
 
 interface LoginResponse {
-	access_token: string
+  access_token: string;
 }
 
 interface SignupRequest {
-	username: string
-	email: string
-	password: string
-	role: string
+  username: string;
+  email: string;
+  password: string;
+  role: string;
 }
 
 interface ErrorResp {
-	error: string
-	code: number
+  error: string;
+  code: number;
 }
 
 interface GetUsernameResponse {
-	username: string
+  username: string;
 }
 
 export async function Signup(
-	username: string,
-	email: string,
-	password: string
+  username: string,
+  email: string,
+  password: string
 ): Promise<number> {
-	const data: SignupRequest = {
-		username,
-		email,
-		password,
-		role: 'user'
-	}
+  const data: SignupRequest = {
+    username,
+    email,
+    password,
+    role: "user",
+  };
 
-	try {
-		const resp = await fetch('http://localhost:3021/api/v1/register', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(data)
-		})
+  try {
+    const resp = await fetch(
+      "https://codeletserver-production.up.railway.app/api/v1/register",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
 
-		if (!resp.ok) {
-			return resp.status
-		}
+    if (!resp.ok) {
+      return resp.status;
+    }
 
-		return 200
-	} catch (err) {
-		console.error(err);
-		return 500
-	}
+    return 200;
+  } catch (err) {
+    console.error(err);
+    return 500;
+  }
 }
 
 export async function Login(email: string, password: string): Promise<number> {
-	const data: LoginRequest = {
-		email,
-		password
-	}
+  const data: LoginRequest = {
+    email,
+    password,
+  };
 
-	try {
-		const resp = await fetch('http://localhost:3021/api/v1/login', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(data)
-		})
+  try {
+    const resp = await fetch(
+      "https://codeletserver-production.up.railway.app/api/v1/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
 
-		if (!resp.ok) {
-			return resp.status
-		}
+    if (!resp.ok) {
+      return resp.status;
+    }
 
-		const token = (await resp.json()) as LoginResponse
-		localStorage.setItem('ACCESS_TOKEN', token.access_token)
+    const token = (await resp.json()) as LoginResponse;
+    localStorage.setItem("ACCESS_TOKEN", token.access_token);
 
-		return 200
-	} catch (err) {
-		console.error(err);
-		return 500
-	}
+    return 200;
+  } catch (err) {
+    console.error(err);
+    return 500;
+  }
 }
 
 export async function Logout(token: string) {
-	if (token === '') return
+  if (token === "") return;
 
-	try {
-		const resp = await fetch(`http://localhost:3021/api/v1/logout`, {
-			method: 'POST',
-			headers: {
-				Authorization: token
-			}
-		})
+  try {
+    const resp = await fetch(
+      `https://codeletserver-production.up.railway.app/api/v1/logout`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
 
-		if (!resp.ok) {
-			const errResp = (await resp.json()) as ErrorResp
-			console.log(errResp)
-			return
-		}
-	} catch (err) {
-		console.error(err)
-		return
-	}
+    if (!resp.ok) {
+      const errResp = (await resp.json()) as ErrorResp;
+      console.log(errResp);
+      return;
+    }
+  } catch (err) {
+    console.error(err);
+    return;
+  }
 }
 
-
 export async function GetUsername(token: string): Promise<string | undefined> {
-	if (token === '') return undefined
+  if (token === "") return undefined;
 
-	try {
-		const resp = await fetch(`http://localhost:3021/api/v1/username`, {
-			method: 'GET',
-			headers: {
-				Authorization: token
-			}
-		})
+  try {
+    const resp = await fetch(
+      `https://codeletserver-production.up.railway.app/api/v1/username`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
 
-		if (!resp.ok) {
-			const errResp = (await resp.json()) as ErrorResp
-			console.log(errResp)
-			return undefined
-		}
-		
-		const info = (await resp.json()) as GetUsernameResponse
+    if (!resp.ok) {
+      const errResp = (await resp.json()) as ErrorResp;
+      console.log(errResp);
+      return undefined;
+    }
 
-		return info.username
-	} catch (err) {
-		console.error(err)
-		return undefined
-	}
+    const info = (await resp.json()) as GetUsernameResponse;
+
+    return info.username;
+  } catch (err) {
+    console.error(err);
+    return undefined;
+  }
 }
